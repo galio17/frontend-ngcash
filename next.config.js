f23df3d2550ff9fd8e940f997978237dcc790662
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-11-20 16:15:53 -0300

    feat: add api configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    BASE_URL: "https://ngcash-giuseppe-test.herokuapp.com",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
