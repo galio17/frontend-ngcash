611d5da487c27dee8b5d3840795cb54c3793d4d3
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-11-20 00:02:11 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+
+import Image from "next/image";
+
+import { FaRegLightbulb } from "react-icons/fa";
+import { useThemeContext } from "../../providers";
+
+interface HeaderProps {
+  children?: ReactNode;
+}
+
+function Header({ children }: HeaderProps) {
+  const { changeTheme, isDark } = useThemeContext();
+
+  const logo = isDark ? "/logo_ng_cash.gif" : "/logo_ng_cash-light.gif";
+
+  return (
+    <header className="shadow-lg dark:bg-grey-2">
+      <div className="container flex justify-between items-center mx-auto">
+        <figure>
+          <Image src={logo} alt="Logo" width={80} height={80} />
+        </figure>
+        <div className="flex-1 flex justify-end items-stretch dark gap-2">
+          {children}
+
+          <button className="btn" onClick={changeTheme}>
+            <FaRegLightbulb className="w-4 h-4" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header>
+        <button className="btn">Cadastrar-se</button>
+      </Header>
+    </div>
+  );
+}
+
+export default Home;
