284636035432c51115b2aacf28733b4e8fa9d2db
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-11-22 11:03:38 -0300

    fix: remove lottie from project cause build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
 
-import animationData from "../assets/json/piggy-bank.json";
 import LoginForm from "../components/FormFields/LoginForm";
 import Header from "../components/Header";
-import Ilustration from "../components/Ilustration";
 
 function Home() {
   return (
@@ -16,10 +14,6 @@ function Home() {
       <section className="flex-1 py-3 px-5 flex">
         <div className="container mx-auto flex justify-around items-center relative">
           <LoginForm />
-          <aside className="flex max-sm:hidden items-center">
-            <Ilustration src={animationData} />
-            <div className="object-fill cursor"></div>
-          </aside>
         </div>
       </section>
     </div>
--- src/pages/signUp/index.tsx
+++ src/pages/signUp/index.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
 
-import animationData from "../../assets/json/piggy-bank.json";
 import SignUpForm from "../../components/FormFields/SignUpForm";
 import Header from "../../components/Header";
-import Ilustration from "../../components/Ilustration";
 
 function SignUp() {
   return (
@@ -15,10 +13,6 @@ function SignUp() {
       </Header>
       <section className="flex-1 py-3 px-5 flex">
         <div className="container mx-auto flex justify-around items-center relative">
-          <aside className="flex max-sm:hidden items-center">
-            <Ilustration src={animationData} />
-            <div className="object-fill cursor"></div>
-          </aside>
           <SignUpForm />
         </div>
       </section>
