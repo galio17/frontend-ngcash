89acd6d2b7c32739983c4e48cd07ab7d13cacddb
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-11-21 23:33:34 -0300

    feat: add transfer and get transation functions to user context

--- src/providers/userContext/index.tsx
+++ src/providers/userContext/index.tsx
@@ -1,19 +1,27 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 import { parseCookies, setCookie } from "nookies";
 
+import { api } from "../../services/api";
+import { socket } from "../../services/websocket";
 import {
   ILoginResponse,
+  ITransaction,
   IUser,
   IUserContext,
   IUserProviderProps,
+  TGetTransaction,
   TLoginFunction,
   TSignUpFunction,
+  TTransferFunction,
 } from "./interfaces";
 
 export const UserContext = createContext<IUserContext>({} as IUserContext);
 
 function UserProvider({ children }: IUserProviderProps) {
+  const [user, setUser] = useState<IUser>({} as IUser);
+  const [transactions, setTransactions] = useState<ITransaction[]>([]);
+
   const login: TLoginFunction = async (userData) => {
     try {
       const { data } = await api.post<ILoginResponse>("/login", userData);
@@ -44,8 +52,60 @@ function UserProvider({ children }: IUserProviderProps) {
     }
   };
 
+  const transfer: TTransferFunction = async (transferData) => {
+    try {
+      const { "@ngcash:token": token } = parseCookies();
+      const authorization = `Bearer ${token}`;
+      api.defaults.headers["Authorization"] = authorization;
+
+      const { data } = await api.post<ITransaction>(
+        "/transactions/transfer",
+        transferData
+      );
+
+      setTransactions([data, ...transactions]);
+      setUser({ ...user, balance: user.balance - data.value });
+
+      const { to, id: transactionId } = data;
+      socket.emit("transferTransaction", { to, transactionId });
+
+      return data;
+    } catch (err) {
+      console.error(err);
+
+      throw err;
+    }
+  };
+
+  const getTransaction: TGetTransaction = async (id) => {
+    try {
+      const { "@ngcash:token": token } = parseCookies();
+      const authorization = `Bearer ${token}`;
+      api.defaults.headers["Authorization"] = authorization;
+
+      const { data } = await api.get<ITransaction>(`/transactions/${id}`);
+
+      return data;
+    } catch (err) {
+      console.error(err);
+
+      throw err;
+    }
+  };
+
   return (
-    <UserContext.Provider value={{ login, signUp }}>
+    <UserContext.Provider
+      value={{
+        login,
+        signUp,
+        transfer,
+        getTransaction,
+        user,
+        setUser,
+        transactions,
+        setTransactions,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
--- src/providers/userContext/interfaces.d.ts
+++ src/providers/userContext/interfaces.d.ts
@@ -1,26 +1,51 @@
-import { ReactNode } from "react";
+import { Dispatch, ReactNode, SetStateAction } from "react";
 
-interface IUser {
-  id: string;
+interface IUserRequest {
   username: string;
-  balance: number;
+  password: string;
 }
 
-interface IUserRequest extends Pick<IUser, "username"> {
-  password: string;
+interface IUser extends Omit<IUserRequest, "password"> {
+  id: string;
+  balance: number;
 }
 
 interface ILoginResponse {
   token: string;
 }
 
-type TLoginFunction = (data: IUserRequest) => Promise<string | void>;
+type TLoginFunction = (data: IUserRequest) => Promise<string>;
 
-type TSignUpFunction = (data: IUserRequest) => Promise<IUser | void>;
+type TSignUpFunction = (data: IUserRequest) => Promise<IUser>;
+
+interface ITransferRequest {
+  to: string;
+  value: number;
+}
+
+interface ITransaction extends ITransferRequest {
+  id: string;
+  from: string;
+  releaseDate: string;
+}
+
+interface ITransferEmit extends Pick<ITransferRequest, "to"> {
+  transactionId: string;
+}
+
+type TTransferFunction = (data: ITransferRequest) => Promise<ITransaction>;
+
+type TGetTransaction = (transactionId: string) => Promise<ITransaction>;
 
 interface IUserContext {
   login: TLoginFunction;
   signUp: TSignUpFunction;
+  transfer: TTransferFunction;
+  getTransaction: TGetTransaction;
+  transactions: ITransaction[];
+  setTransactions: Dispatch<SetStateAction<ITransaction[]>>;
+  user: IUser;
+  setUser: Dispatch<SetStateAction<IUser>>;
 }
 
 interface IUserProviderProps {
